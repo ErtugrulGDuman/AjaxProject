0191f87c94fc93c3830b45266e67ff9411000ff4
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-06-25 21:53:57 +0300

    Add project files.

---
+++ AjaxProject/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AjaxProject.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=EGD\\SQLEXPRESS;initial Catalog=AjaxProject;integrated Security=true");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
